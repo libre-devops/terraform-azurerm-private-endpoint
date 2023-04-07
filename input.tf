57d9fb0a40edd43fe2091555adae63f6f72facaa
Craig Thacker <craigthackerx@gmail.com>
2023-04-07 15:30:17 +0100

    Update module

--- input.tf
+++ input.tf
@@ -1,14 +1,96 @@
-variable "identity_ids" {
-  description = "Specifies a list of user managed identity ids to be assigned to the VM."
-  type        = list(string)
-  default     = []
+variable "private_service_connection" {
+  type = any
+  description = "The private_service_connection block"
+  default = {}
 }
 
-variable "identity_type" {
-  description = "The Managed Service Identity Type of this Virtual Machine."
-  type        = string
-  default     = ""
+variable "private_endpoint_name" {
+  type = string
+  description = "The name of the private endpoint"
 }
+
+variable "subnet_id" {
+  type = string
+  description = "The ID of the subnet the private endpoint needs to connect"
+}
+
+variable "custom_network_interface_name" {
+  type = string
+  description = "The name of your private endpoint NIC"
+  default = null
+}
+
+variable "private_dns_zone_group" {
+  type = any
+  description = "The private_dns_zone_group block"
+  default = null
+}
+
+variable "sub_resource_names" {
+  type = map(string)
+  description = "The sub resource names of private endpoints found at https://learn.microsoft.com/en-gb/azure/private-link/private-endpoint-overview#private-link-resource, not used, but provided for lookup option"
+  default = {
+    "Microsoft.Appconfiguration/configurationStores" = "configurationStores"
+    "Microsoft.Automation/automationAccounts" = "Webhook, DSCAndHybridWorker"
+    "Microsoft.AzureCosmosDB/databaseAccounts" = "SQL, MongoDB, Cassandra, Gremlin, Table"
+    "Microsoft.Batch/batchAccounts" = "batchAccount, nodeManagement"
+    "Microsoft.Cache/Redis" = "redisCache"
+    "Microsoft.Cache/redisEnterprise" = "redisEnterprise"
+    "Microsoft.CognitiveServices/accounts" = "account"
+    "Microsoft.Compute/diskAccesses" = "managed disk"
+    "Microsoft.ContainerRegistry/registries" = "registry"
+    "Microsoft.ContainerService/managedClusters" = "management"
+    "Microsoft.DataFactory/factories" = "dataFactory"
+    "Microsoft.Kusto/clusters" = "cluster"
+    "Microsoft.DBforMariaDB/servers" = "mariadbServer"
+    "Microsoft.DBforMySQL/servers" = "mysqlServer"
+    "Microsoft.DBforPostgreSQL/servers" = "postgresqlServer"
+    "Microsoft.Devices/provisioningServices" = "iotDps"
+    "Microsoft.Devices/IotHubs" = "iotHub"
+    "Microsoft.IoTCentral/IoTApps" = "IoTApps"
+    "Microsoft.DigitalTwins/digitalTwinsInstances" = "API"
+    "Microsoft.EventGrid/domains" = "domain"
+    "Microsoft.EventGrid/topics" = "topic"
+    "Microsoft.EventHub/namespaces" = "namespace"
+    "Microsoft.HDInsight/clusters" = "cluster"
+    "Microsoft.HealthcareApis/services" = "fhir"
+    "Microsoft.Keyvault/managedHSMs" = "HSM"
+    "Microsoft.KeyVault/vaults" = "vault"
+    "Microsoft.MachineLearningServices/workspaces" = "amlworkspace"
+    "Microsoft.Migrate/assessmentProjects" = "project"
+    "Microsoft.Network/applicationgateways" = "application gateway"
+    "Microsoft.Network/privateLinkServices" = "empty"
+    "Microsoft.PowerBI/privateLinkServicesForPowerBI" = "Power BI"
+    "Microsoft.Purview/accounts" = "account, portal"
+    "Microsoft.RecoveryServices/vaults" = "AzureBackup, AzureSiteRecovery"
+    "Microsoft.Relay/namespaces" = "namespace"
+    "Microsoft.Search/searchServices" = "searchService"
+    "Microsoft.ServiceBus/namespaces" = "namespace"
+    "Microsoft.SignalRService/SignalR" = "signalr"
+    "Microsoft.SignalRService/webPubSub" = "webpubsub"
+    "Microsoft.Sql/servers" = "SQL Server (sqlServer)"
+    "Microsoft.Storage/storageAccounts" = "blob, blob_secondary, table, table_secondary, queue, queue_secondary, file, file_secondary, web, web_secondary, dfs, dfs_secondary"
+    "Microsoft.StorageSync/storageSyncServices" = "File Sync Service"
+    "Microsoft.Synapse/privateLinkHubs" = "web"
+    "Microsoft.Synapse/workspaces" = "Sql, SqlOnDemand, Dev"
+    "Microsoft.Web/hostingEnvironments" = "hosting environment"
+    "Microsoft.Web/sites" = "sites"
+    "Microsoft.Web/staticSites" = "staticSites"
+    "Microsoft.Media/mediaservices" = "keydelivery, liveevent, streamingendpoint"
+    "Microsoft.Authorization/resourceManagementPrivateLinks" = "ResourceManagement"
+    "Microsoft.Databricks/workspaces" = "databricks_ui_api, browser_authentication"
+    "Microsoft.Insights/privatelinkscopes" = "azuremonitor"
+  }
+}
+
+
+
+variable "ip_configuration" {
+  type = any
+  description = "The ip configuration block"
+  default = null
+}
+
 
 variable "location" {
   description = "The location for this resource to be put in"
--- output.tf
+++ output.tf
@@ -1,32 +1,30 @@
-output "sa_id" {
-  value       = azurerm_storage_account.sa.id
-  description = "The ID of the storage account"
+output "endpoint_id" {
+  value       = azurerm_private_endpoint.endpoint.id
+  description = "The ID of the private endpoint"
 }
 
-output "sa_name" {
-  value       = azurerm_storage_account.sa.name
+output "endpoint_name" {
+  value       = azurerm_private_endpoint.endpoint.name
   description = "The name of the storage account"
 }
 
-output "sa_primary_access_key" {
-  value       = azurerm_storage_account.sa.primary_access_key
-  description = "The primary access key of the storage account"
-  sensitive   = true
+output "endpoint_network_interface" {
+  value       = azurerm_private_endpoint.endpoint.network_interface
+  description = "The network interface block"
 }
 
-output "sa_primary_blob_endpoint" {
-  value       = azurerm_storage_account.sa.primary_blob_endpoint
-  description = "The primary blob endpoint of the storage account"
+
+output "endpoint_custom_dns_configs" {
+  value       = azurerm_private_endpoint.endpoint.custom_dns_configs
+  description = "The custom dns configs block"
 }
 
-output "sa_primary_connection_string" {
-  value       = azurerm_storage_account.sa.primary_blob_connection_string
-  description = "The primary blob connection string of the storage account"
-  sensitive   = true
+output "endpoint_private_dns_zone_configs" {
+  value       = azurerm_private_endpoint.endpoint.private_dns_zone_configs
+  description = "The private dns zone configs"
 }
 
-output "sa_secondary_access_key" {
-  value       = azurerm_storage_account.sa.secondary_access_key
-  description = "The secondary access key of the storage account"
-  sensitive   = true
+output "endpoint_ip_configuration" {
+  value       = azurerm_private_endpoint.endpoint.ip_configuration
+  description = "The ip configuration block"
 }
